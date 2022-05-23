d566b471b9c70db5298c0b2d58c2a8e527a9a010
Akın <82474005+acupofgreentea@users.noreply.github.com>
2022-05-23 14:37:32 +0300

    Enemy Heal, Player Debuff

--- ZeroDoubt/Assets/0_Scripts/Enemy.cs
+++ ZeroDoubt/Assets/0_Scripts/Enemy.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : Character
 {
@@ -15,28 +16,63 @@ public class Enemy : Character
     private void OnMouseDown()
     {
         player.CurrentEnemy = this;
-        
-        player.StartCoroutine(player.PlayerAttack());
+
+        SetPlayerSkill();
+    }
+
+    private void SetPlayerSkill()
+    {
+        switch (player.SkillIndex)
+        {
+            case 0:
+                player.Attack();
+                break;
+            case 1:
+                player.DebuffEnemy(1);
+                break;
+        }
     }
 
     public void ChooseBehaviour()
     {
-        StartCoroutine(Attack());
+        StartCoroutine(TurnChangeRoutine());
     }
 
-    public IEnumerator Attack()
+    private void Attack()
     {
-        if (TurnCompleted) yield break;
+        if (TurnCompleted) return;
         
-        battleSystem.ChangeGeneralText(this.characterName + " dealt You " + Damage + " Damage. Be Careful!");
+        battleSystem.ChangeGeneralText(this.CharacterName + " dealt You " + Damage + " Damage. Be Careful!");
         
         var isDead = player.TakeDamage(Damage);
 
         TurnCompleted = true;
+    }
 
+    public IEnumerator TurnChangeRoutine()
+    {
+        // call skill method here
+
+        var skill = Random.Range(0, 2);
+
+        switch (skill)
+        {
+            case 0:
+                Attack();
+                break;
+            
+            case 1:
+                if(CurrentHp == MaxHp)
+                   Attack();
+                else
+                    Heal(); 
+                
+                break;
+        }
+        
         yield return new WaitForSeconds(2f);
         
-        if (isDead)
+        if (player.CurrentHp <= 0)
         {
             battleSystem.BattleState = BattleState.Lost;
             battleSystem.ChangeGeneralText("You Lost :(");
@@ -52,6 +88,19 @@ public class Enemy : Character
 
     private void Heal()
     {
+        if (TurnCompleted) return;
         
+        if (MaxHp >= CurrentHp + healAmount)
+        {
+            CurrentHp += healAmount;
+            battleSystem.ChangeGeneralText(this.CharacterName + " healed themselves for " + healAmount + ".");
+        }
+        else
+        {
+            battleSystem.ChangeGeneralText(this.CharacterName + " healed themselves for " + (MaxHp - CurrentHp) + ".");
+            CurrentHp = MaxHp;
+        }
+        
+        SetHealthBar();
     }
 }
--- ZeroDoubt/Assets/0_Scripts/Player.cs
+++ ZeroDoubt/Assets/0_Scripts/Player.cs
@@ -1,31 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Player : Character
 {
     public Enemy CurrentEnemy { get; set; }
     
+    public int SkillIndex { get; private set; }
+
+    [SerializeField] private TextMeshProUGUI attackDamageText;
+    [SerializeField] private TextMeshProUGUI healAmountText;
+
     protected override void Start()
     {
         base.Start();
+
+        attackDamageText.text = Damage.ToString();
+        healAmountText.text = healAmount.ToString();
     }
 
-    public void ChooseAnEnemyToAttack()
+    public void ChooseAnEnemy(int index)
     {
         if (battleSystem.BattleState != BattleState.PlayerTurn) return;
-        
+
+        SkillIndex = index;
+
         battleSystem.ChangeGeneralText("Choose an Enemy to Attack!");
     }
 
-    public IEnumerator PlayerAttack()
+    public void Attack()
     {
-        if (battleSystem.BattleState != BattleState.PlayerTurn) yield break;
-        
-        if(TurnCompleted) yield break;
+        if (battleSystem.BattleState != BattleState.PlayerTurn) return;
+
+        if (TurnCompleted) return;
 
         var isDead = CurrentEnemy.TakeDamage(Damage);
-        
+
         battleSystem.ChangeGeneralText("You Dealt " + Damage + " Damage to Enemy! Good Job");
 
         TurnCompleted = true;
@@ -36,64 +47,72 @@ public class Player : Character
             CurrentEnemy.gameObject.SetActive(false);
         }
 
-        yield return new WaitForSeconds(2f);
-        
-        TurnCompleted = false;
-        
-        if (isDead)
-        {
-            if (battleSystem.EnemiesList.Count != 0)
-            {
-                battleSystem.ChangeGeneralText("Enemy Turn");
-                battleSystem.BattleState = BattleState.EnemyTurn;
-                battleSystem.EnemyTurn();
-            }
-            else
-            {
-                battleSystem.BattleState = BattleState.Won;
-                battleSystem.ChangeGeneralText("You won the game!");
-            }
-        }
-        else
-        {
-            battleSystem.BattleState = BattleState.EnemyTurn;
-            battleSystem.ChangeGeneralText("Enemy Turn");
-            battleSystem.EnemyTurn();
-        }
+        StartCoroutine(TurnChangeRoutine());
     }
     
-    public void HealPlayer()
+    public void Heal()
     {
         if (battleSystem.BattleState != BattleState.PlayerTurn) return;
-        
-        if(TurnCompleted) return;
-        
-        if(CurrentHP == MaxHP)
+
+        if (TurnCompleted) return;
+
+        if (CurrentHp == MaxHp)
             battleSystem.ChangeGeneralText("You are on Max HP. You can't heal yourself! Choose an another action.");
         else
         {
-            StartCoroutine(HealPlayerRoutine());
+            if (MaxHp >= CurrentHp + healAmount)
+            {
+                battleSystem.ChangeGeneralText("You Healed " + healAmount + " Yourself. Wise Choice!");
+                CurrentHp += healAmount;
+            }
+            else
+            {
+                battleSystem.ChangeGeneralText("You Healed " + (MaxHp - CurrentHp) + " Yourself. Wise Choice!");
+                CurrentHp = MaxHp;
+            }
+            
+            SetHealthBar();
+
+            
+            
+            StartCoroutine(TurnChangeRoutine());
         }
     }
 
-    private IEnumerator HealPlayerRoutine()
+    public void DebuffEnemy(int damageDecrease)
     {
-        if (MaxHP >= CurrentHP + healAmount)
-            CurrentHP += healAmount;
+        if (battleSystem.BattleState != BattleState.PlayerTurn) return;
+
+        if (TurnCompleted) return;
+
+        if (CurrentEnemy.Damage >= 0)
+            CurrentEnemy.Damage -= damageDecrease;
         else
-            CurrentHP = MaxHP;
+            CurrentEnemy.Damage = 0;
         
-        SetHealthBar();
-        
-        battleSystem.ChangeGeneralText("You Healed " + healAmount + " Yourself. Wise Choice!");
-        
+        battleSystem.ChangeGeneralText(CurrentEnemy.CharacterName + "'s damage has " + damageDecrease + " decreased. Now They are pissed!!");
+
+        TurnCompleted = true;
+
+        StartCoroutine(TurnChangeRoutine());
+    }
+    
+    public IEnumerator TurnChangeRoutine()
+    {
         yield return new WaitForSeconds(2f);
-        
-        battleSystem.BattleState = BattleState.EnemyTurn;
-        battleSystem.ChangeGeneralText("Enemy Turn");
 
         TurnCompleted = false;
         
-        battleSystem.EnemyTurn();
+        if (battleSystem.EnemiesList.Count != 0)
+        {
+            battleSystem.ChangeGeneralText("Enemy Turn");
+            battleSystem.BattleState = BattleState.EnemyTurn;
+            battleSystem.EnemyTurn();
+        }
+        else
+        {
+            battleSystem.BattleState = BattleState.Won;
+            battleSystem.ChangeGeneralText("You won the game!");
+        }
     }
 }
